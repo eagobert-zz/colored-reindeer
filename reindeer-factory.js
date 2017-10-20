cf9c0fca6bfa9355edb22d32079fb50fc68bb3b7
eagobert <eagobert@hotmail.com>
2017-10-20 15:15:02 -0500

    Finally got the factory to work.

--- reindeer-factory.js
+++ reindeer-factory.js
@@ -1,31 +1,35 @@
-
 //What do I want to  do? Uhhh, well create an object that cycles through the "reindeer" and "color" arrays, picks the current property of each and places in an individual object.
 
 //Step 1:  Created a const named "coloredReindeerFactory" to generate my object.
-const coloredReindeerBuilder = function () {
-
-        //Step 2:  Create the "for" loop to loop through the "reindeer" array. Why just thru the "reindeer" array?  Because I only need a color for each reindeer even though the "color" array is longer.
-        const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+const coloredReindeerBuilder = function* () {
     
-        const color = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
+            //Step 2:  Create the "for" loop to loop through the "reindeer" array. Why just thru the "reindeer" array?  Because I only need a color for each reindeer even though the "color" array is longer.
+            const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+        
+            const color = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
+        
+            for(i = 0; i < reindeer.length; i++) {
+                let currentReindeer = reindeer[i]
+                let currentColor = color[i]
     
-        for(i = 0; i < reindeer.length; i++) {
-            let currentReindeer = reindeer[i]
-            let currentColor = color[i]
+            //Step 3:  Create the foundation of my object to include "reindeer" and "color" values
+                yield Object.create (null, {
+                    "Reindeer": {
+                        enumerable: true,
+                        value: currentReindeer
+                    },
+        
+                    "Color": {
+                        enumerable: true,
+                        value: currentColor
+                    }
+                })
+            }
+    }
 
-        //Step 3:  Create the foundation of my object to include "reindeer" and "color" values
-            return Object.create (null, {
-                "Reindeer": {
-                    enumerable: true,
-                    value: currentReindeer
-                },
-    
-                "Color": {
-                    enumerable: true,
-                    value: currentColor
-                }
-            })
-        }
-}
+    coloredReindeerArray = coloredReindeerBuilder();
 
-console.log(coloredReindeerBuilder());
+    console.log(coloredReindeerArray.next().value);
+    console.log(coloredReindeerArray.next().value);
+    console.log(coloredReindeerArray.next().value);
+    console.log(coloredReindeerArray.next().value);
