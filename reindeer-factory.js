767553b7b29d3929ecbee1a421606d6d93ec8205
eagobert <eagobert@hotmail.com>
2017-10-20 11:48:21 -0500

    Refactored factory function

--- reindeer-factory.js
+++ reindeer-factory.js
@@ -1,30 +1,29 @@
+
+//What do I want to  do? Uhhh, well create an object that cycles through the "reindeer" and "color" arrays, picks the current property of each and places in an individual object.
+
+//Step 1:  Created a const named "coloredReindeerFactory" to generate my object.
 const coloredReindeerBuilder = function () {
 
-    // Write a for loop that looks at each reindeer
-    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+        //Step 2:  Create the "for" loop to loop through the "reindeer" array. Why just thru the "reindeer" array?  Because I only need a color for each reindeer even though the "color" array is longer.
+        const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    
+        const color = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
+    
+        for(i = 0; i < reindeer.length; i++) {
+            let currentReindeer = reindeer[i]
+            let currentColor = color[i]
 
-    for(i = 0; i < reindeer.length; i++) {
-        currentReindeer = reindeer[i]
-        console.log(currentReindeer); 
-    }
+        }
+        //Step ?:  Create the foundation of my object to include "reindeer" and "color"
+        return object.create (null, {
+            "Reindeer": {
+                enumerable: true,
+                value: currentReindeer
+            },
 
-    //Write a for loop that looks at each color
-    const color = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
-
-    for(j = 0; j < color.length; j++) {
-        currentColor = color[j]
-        console.log(currentColor);
-    }
-
-
-
-
-    // Invoke factory function to create reindeer object
-
-        // Put new reindeer object in coloredReindeer array
-
-
-    // Return coloredReindeer array
+            "Color": {
+                enumerable: true,
+                value: currentColor
+            }
+        })
 }
-
-coloredReindeerBuilder();
